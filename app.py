4a7d3defcaec58b2e91cf9010d899f5d2ee51cbb
bl815v <133074441+bl815v@users.noreply.github.com>
2024-05-21 07:27:11 -0500

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app=Flask(__name__, template_folder='template', static_folder='static')
+app.secret_key = 'clave'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
