61791395cacb7f275924193f2455174a4cb9ff54
jaidergarzon259 <160967646+jaidergarzon259@users.noreply.github.com>
2024-05-27 07:35:56 -0500

    Add files via upload

---
+++ Logic/Combate/juego.py
@@ -0,0 +1,5 @@
+Charmander = ["charmander","fuego",100,50,40,["Agua"],["Planta", "Fuego"]]
+Pikachu = ["pikachu","electrico",95,50,40,["Roca"],["Electrico"]]
+
+while(Charmander[3] > 0 and Pikachu[3] > 0):
+    print("hola")
