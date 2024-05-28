e8f5887f6e56e00b9046f7c83dce0e8e78c65d5d
jaidergarzon259 <160967646+jaidergarzon259@users.noreply.github.com>
2024-05-28 17:23:10 -0500

    Update juego.py

--- Logic/Combate/juego.py
+++ Logic/Combate/juego.py
@@ -1,5 +1,1 @@
-Charmander = ["charmander","fuego",100,50,40,["Agua"],["Planta", "Fuego"]]
-Pikachu = ["pikachu","electrico",95,50,40,["Roca"],["Electrico"]]
 
-while(Charmander[3] > 0 and Pikachu[3] > 0):
-    print("hola")
