b88b579f62f28fd500ed3815cafeb056e50dcc20
bl815v <133074441+bl815v@users.noreply.github.com>
2024-05-31 09:41:56 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template, send_from_directory, url_for
+from flask import Flask, render_template, request, send_from_directory
+from flask_mysqldb import MySQL
 
+from data.Admin_base import Admin_base
 from logic.Estilo import Estilo
+from logic.Usuario import Usuario
+from logic.combate.Juego import Juego
 
 app=Flask(__name__, template_folder='web/template', static_folder='web/static')
 app.secret_key = 'clave'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '151709Jebl'
+app.config['MYSQL_DB'] = 'batallasPokemon'
+
+mysql = MySQL(app)
+
+admin_base = Admin_base(mysql)
+
+@app.route('/usuarios')
+def usuarios():    
+    usuarios = admin_base.getUsuarios()
+    return render_template('usuarios.html', usuarios=usuarios)
 
 
 @app.route('/favicon.ico')
@@ -19,6 +37,22 @@ def index():
 def registro():
     background_url = Estilo.obtener_fondo_gif('inicial')
     return render_template('registro.html', background_url=background_url)
+
+@app.route('/RegistroExitoso', methods=['POST'])
+def RegistroExitoso():
+    nombre = request.form.get('nombre')
+    correo = request.form.get('correo')
+    clave = request.form.get('clave')
+    repetirclave = request.form.get('repetirclave')
+
+    if clave != repetirclave:
+        return "Las contraseñas no coinciden. Inténtalo de nuevo."
+
+    nuevo_usuario = Usuario(nombre, correo, clave)
+
+    admin_base.guardarUsuario(nuevo_usuario)
+    background_url = Estilo.obtener_fondo_gif('inicial')
+    return render_template('index.html', background_url=background_url)
 
 @app.route('/usuario')
 def usuario():
@@ -38,6 +72,7 @@ def perdiste():
 
 @app.route('/batalla')
 def batalla():
+    Iniciarjuego = Juego()
     background_url = Estilo.obtener_fondo_png('batalla')
     return render_template('batalla.html', background_url=background_url)
 
