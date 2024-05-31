a58a86f67679786b505f28a831da794dfc12b92c
Juan <150971486+Zarju@users.noreply.github.com>
2024-05-30 23:34:34 -0500

    Delete logic/pokemon directory

--- logic/pokemon/tipos/PokemonFuego.py
+++
@@ -1,7 +0,0 @@
-from Logic.Pokemon.PokemonBase import Pokemon
-
-class PokemonFuego(Pokemon):
-    def __init__(self, tipo, nombre, vida,ataque,defensa, debilidad, resistencia):
-        super().__init__(tipo, nombre, vida,ataque,defensa, debilidad, resistencia)
-         
-    
